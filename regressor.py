aa3a3fb20efaec5cb56f7822cd709d95d6442827
David John Gagne II <djgagne@ou.edu>
2015-09-26 11:37:38 -0600

    Modified feature extractor

--- regressor.py
+++ regressor.py
@@ -4,7 +4,7 @@ from sklearn.base import BaseEstimator
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = GradientBoostingRegressor(n_estimators=100, max_features="sqrt", max_depth=5)
+        self.clf = GradientBoostingRegressor(n_estimators=100, max_features="sqrt", max_depth=6)
 
     def fit(self, X, y):
         self.clf.fit(X, y.ravel())
--- ts_feature_extractor.py
+++ ts_feature_extractor.py
@@ -1,18 +1,15 @@
-
 class FeatureExtractor(object):
-
     def __init__(self):
         pass
 
+    def fit(self, temperatures_xray, n_burn_in, n_lookahead):
+        pass
+
     def transform(self, temperatures_xray, n_burn_in, n_lookahead, skf_is):
-        """Compute the single variable of mean temperatures in the El Nino 3.4 
-        region."""
-        # This is the range for which features should be provided. Strip
-        # the burn-in from the beginning and the prediction look-ahead from
-        # the end.
-        valid_range = range(n_burn_in, temperatures_xray['time'].shape[0] - n_lookahead)
-        enso = get_enso_mean(temperatures_xray['tas'])
-        enso_valid = enso.values[valid_range]
-        X = enso_valid.reshape((enso_valid.shape[0], 1))
-        # trying to cheat by looking ahead
-        return X
+        """Use world temps as features."""
+        # Set all temps on world map as features
+        all_temps = temperatures_xray['tas'].values
+        time_steps, lats, lons = all_temps.shape
+        all_temps = all_temps.reshape((time_steps, lats * lons))
+        all_temps = all_temps[n_burn_in:-n_lookahead, :]
+        return all_temps
